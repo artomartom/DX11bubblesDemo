505b77c352d9d923a973f947c6a43839b6d7c294
artomartom <1tematematema@gmail.com>
2022-05-05 02:12:53 +0300

    working on bubble appearance

--- Source/Shader/Source/Func.hlsl
+++ Source/Shader/Source/Func.hlsl
@@ -1,7 +1,37 @@
-
-
-float inverselerp (float a,float b,float v   )
+#define PI 3.14159265358979323846264338327950288419716939937510
+float InverseLerp (float a,float b,float v   )
 {
 return (v-a)/(b-a);
 };
+
+//   
+float smootherstep (float a0, float a1, float w) 
+{
+  return (a1 - a0) * ((w * (w * 6.0 - 15.0) + 10.0) * w * w * w) + a0;
+}
+
+   
+float2 smootherstep (float2 a0, float2 a1, float2 w) 
+{
+  return (a1 - a0) * ((w * (w * 6.0 - 15.0) + 10.0) * w * w * w) + a0;
+}
+   
+float3 smootherstep (float3 a0, float3 a1, float3 w) 
+{
+  return (a1 - a0) * ((w * (w * 6.0 - 15.0) + 10.0) * w * w * w) + a0;
+}
+
+float Psudorand (float a  )
+{
+    return 1;
+};
  
+float plot (float2 st, float pct){
+  return  smoothstep( pct-0.01, pct, st.y) -
+          smoothstep( pct, pct+0.01, st.y);
+}
+
+float plot (float2 st, float pct,float width ){
+  return  smoothstep( pct-width, pct, st.y) -
+          smoothstep( pct, pct+width, st.y);
+}
--- Source/Shader/Source/main.hlsl
+++ Source/Shader/Source/main.hlsl
@@ -6,10 +6,53 @@
 RWTexture2D<circleTexFormat> tex : register(u0);
  
 static const uint2  circleSize =uint2( 280, 280);
-static const float  maxIntensity= 0.5f;
+static const float  maxIntensity= 0.2f;
 static const float  maxDistance= 1.41421;
 static const float  edge= 0.96f;
-static const float  edgeWidth= (1.f - edge);
+static const float  outerEdgeWidth= 1.0f - edge;
+static const float  InnerEdgeWidth= edge - 0.83f;
+static const float3  ColorA= float3(0.941176534f, 1.000000000f, 1.000000000f );
+static const float3  ColorB= float3(0.545098066f, 0.000000000f, 0.000000000f );
+static const float3  ColorC= float3(0.862745166f, 0.862745166f, 0.862745166f );
+
+/*
+
+float circle(vec2 pos ,float radius   )
+{
+    float distance=length(pos);
+    return 1.0 - smoothstep(0.0, radius, distance);
+}
+  
+float ring(vec2 pos ,float InnerEdge ,float OuterEdge )
+{
+float edge= mix(InnerEdge,OuterEdge,0.5);
+float distance = length(pos);
+return  smoothstep(InnerEdge, edge,distance )-smoothstep( edge, OuterEdge ,distance);
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    vec2 uv = fragCoord/iResolution.xy;
+    vec2 pos =(uv*2.0-1.0 )*vec2(iResolution.x/iResolution.y,1.0) ;
+    
+    //_____
+    vec2 blick1 = vec2(0.2,.4);  
+    vec2 blick2 = vec2(-0.61,-.4);
+  
+   
+    float intens = ring(pos, 0.77,0.97);
+    float innerring = ring(pos, .1,0.97);
+    float outerring = ring(vec2(1.7,.7)*pos-vec2(0.05,-0.05), -1.,0.95);
+    intens+=smoothstep(0.78,1., innerring-outerring);
+    intens+=0.7*circle(pos-blick1,0.1 );
+    intens+=0.7*circle(pos-blick2,0.2  );
+    fragColor = vec4(vec3(intens), 1.0);
+     
+       
+} 
+*/
+
+
 //                                                                                                                          CircleCompute
 [numthreads(circleSize.x, 1, 1)] void CircleCompute( 
       uint3 dispatchThreadId
@@ -18,20 +61,30 @@ static const float  edgeWidth= (1.f - edge);
     if (dispatchThreadId.x >= circleSize.x || dispatchThreadId.y >= circleSize.y)
         return;
 
-    float2 Pos = float2((float(dispatchThreadId.x) / circleSize.x) * 2.f - 1.f,
+    float2 pos = float2((float(dispatchThreadId.x) / circleSize.x) * 2.f - 1.f,
                         (float(dispatchThreadId.y) / circleSize.y) * 2.f - 1.f);
 
-    float distance = length(Pos);
+    float distance = length(pos);
 
-    if (distance > edge + edgeWidth)
+    if (distance > edge + outerEdgeWidth)
     {
         tex[dispatchThreadId.xy] = float4(0.f,0.f,0.f,0.f);
     }
     else
     {
-        float l = smoothstep( edge - edgeWidth , edge  ,distance )-smoothstep( edge, edge + edgeWidth ,distance );
 
-        tex[dispatchThreadId.xy] = float4(1.0f,1.0f,1.0f, l);
+        //  pct = pow(distance(st,vec2(0.460,0.450)),distance(st,vec2(0.290,0.650)));  
+        //float f = abs(sin(a*234.5))*0.55+.3;
+
+        //float intens =  smoothstep( edge - InnerEdgeWidth , edge  ,distance )-smoothstep( edge , edge + outerEdgeWidth ,distance );
+        float2 dir= normalize(  pos);
+        float intens =  smoothstep( edge - InnerEdgeWidth, edge, distance )-smoothstep( edge , edge + outerEdgeWidth ,distance );
+        intens -=(  distance* distance* distance* distance);
+      //  float3 col = ColorB* clamp(  -dir.y , dir.x  ,ColorA);
+      //  float blick =dot(pos,float2(-cos(0.785398163f),sin(0.785398163f)  ));
+        //col+=(lerp(0.9f,1.f, blick.x)-0.9f).x;  
+
+        tex[dispatchThreadId.xy] = float4(lerp(ColorB,ColorA,0.5f), maxIntensity*intens);
     };
 }
 // end circle
